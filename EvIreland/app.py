d7048727cbd157beb7e00b4a834e13107bee9e80
Liang Yu <161745821+Frankyu17@users.noreply.github.com>
2026-03-01 00:00:30 +0000

    Update app.py

--- EvIreland/app.py
+++ EvIreland/app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import json
 import os
+
 def load_clean_json(filename):
     base_path = os.path.dirname(__file__)
     full_path = os.path.join(base_path, filename)
-    
     try:
         with open(full_path, 'r', encoding='utf-8') as f:
             return json.load(f)
     except FileNotFoundError:
-
-        st.sidebar.error(f"⚠️ File Not Found: {filename}")
+        st.sidebar.error(f"File Not Found: {filename}")
         st.sidebar.info(f"Looking in: {full_path}")
         return None
+
 st.set_page_config(
     page_title="Ireland EV Market Analysis",
     layout="wide",
@@ -156,3 +156,4 @@ with col_mid3:
 st.markdown("<br>", unsafe_allow_html=True)
 
 st.caption("Data provided by Central Statistics Office (CSO) Ireland. All charts interactive.")
+
